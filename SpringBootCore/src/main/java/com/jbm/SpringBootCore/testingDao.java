8fe45631db211c0c98dc8b5e5b8dc99edcd87385
25073 <25073@LAPTOP-JB>
2020-10-27 08:44:41 +0200

    Modified some files

--- SpringBootCore/src/main/java/com/jbm/SpringBootCore/testingDao.java
+++ SpringBootCore/src/main/java/com/jbm/SpringBootCore/testingDao.java
@@ -3,6 +3,7 @@ package com.jbm.SpringBootCore;
 public class testingDao {
 	void showWelcomeMessage() {
 	System.out.println("Thank you");
+	System.out.println("I really appreciate the way you work");
 	}
 
 }
