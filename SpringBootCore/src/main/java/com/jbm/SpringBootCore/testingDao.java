83d112823f52a6e97eeff9f0967ff160a4076f49
25073 <25073@LAPTOP-JB>
2020-10-27 09:13:00 +0200

    Conflict resolution-2

--- SpringBootCore/src/main/java/com/jbm/SpringBootCore/testingDao.java
+++ SpringBootCore/src/main/java/com/jbm/SpringBootCore/testingDao.java
@@ -4,6 +4,7 @@ public class testingDao {
 	void showWelcomeMessage() {
 	System.out.println("Thank you");
 	System.out.println("We have completed this....But what next...");
+	System.out.println("Checking conflict resolution-2");
 
 	}
 
